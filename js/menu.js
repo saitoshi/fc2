b287b50e0fcefdd38b9056c793e467368cdba350
Shun <47809066+saitoshi@users.noreply.github.com>
2022-08-07 09:14:14 +0900

    update design

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,16 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
 }
 
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
 }
-
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 window.onscroll = function () { scrollFunction() };
